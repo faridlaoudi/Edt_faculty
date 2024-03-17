dc9db8fd320f0ab1ad2045e9100edf65e16181a0
faridlaoudi <farid.laoudi@etu.usthb.dz>
2024-03-17 18:40:17 +0100

    login

--- loginpermission.js
+++ loginpermission.js
@@ -3,3 +3,12 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/login", (req, res) => {
+    res.json("Login raw ymchi");
+});
+app.get("/permission", (req, res) => {
+    res.json("Permission");
+});
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
