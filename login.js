ef2c152440ec18af2adf511755275fa306473a8f
faridlaoudi <farid.laoudi@etu.usthb.dz>
2024-03-22 16:01:35 +0100

    login detaills

--- login.js
+++ login.js
@@ -1,10 +1,66 @@
-const express = require('express')
-const app = express()
+import express from "express";
+import bcrypt from "bcrypt";
 
-app.get('/', (req, res) => {
-  console.log('here')
-  res.download('L2-ING-INF-MAJ.pdf')
-  res.status(500).send('Hello World!')
-})
+const app = express();
+const port = 3000;
 
-app.listen(3000)
+// In-memory
+const users = [
+  {
+    email: 'farid@gmail.com',
+    password: '$2b$10$BNWWIk1gDEAhqneCdwHfGuSmTaP2xsDn/C.dqoqVtAv3yKq6CSg0K',
+    role: "admin"
+  },
+  {
+    email: "sohieb@gmail.com", password: "$2b$10$BNWWIk1gDEAhqneCdwHfGuSmTaP2xsDn/C.dqoqVtAv3yKq6CSg0K", 
+    role: "student"
+  },
+  {
+    email: "mohanned@gmail.com", 
+    password: "$2b$10$BNWWIk1gDEAhqneCdwHfGuSmTaP2xsDn/C.dqoqVtAv3yKq6CSg0K", 
+    role: "teacher"
+  }
+];
+
+app.use(express.json());
+/* 
+app.post("/register", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // Find user
+    const findUser = users.find((data) => email == data.email);
+    if (findUser) {
+      res.status(400).send("Wrong email or password !");
+    }
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    users.push({ email, password: hashedPassword });
+    console.log(users)
+    res.status(201).send("Registered successfully!");
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+}); */
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // Find user
+    const findUser = users.find((data) => email == data.email);
+    if (!findUser) {
+      res.status(400).send("Wrong email or password !");
+    }
+    const passwordMatch = await bcrypt.compare(password, findUser.password);
+    if(passwordMatch) {
+        res.status(200).send("Logged in successfully!")
+    } else {
+        res.status(400).send("Wrong email or password !");
+    }
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server is started on port 3000");
+});
