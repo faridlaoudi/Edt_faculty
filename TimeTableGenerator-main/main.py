3760087ec9ddd3bf0fc979bc36d2dcf76194b47d
Farid LAOUDI <117023870+faridlaoudi@users.noreply.github.com>
2024-05-22 13:34:58 +0100

    Delete TimeTableGenerator-main directory

--- TimeTableGenerator-main/main.py
+++
@@ -1,6 +0,0 @@
-from schedule_manager import ScheduleManager
-
-def generate_schedules_for_all(years, rooms, teachers):
-    schedule_manager = ScheduleManager(years, rooms, teachers)
-    schedules = schedule_manager.generate_schedules_for_all()
-    return schedules
